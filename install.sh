5ea924003668cb975eac65262d2b983a149202b2
Jerry Reptak <jreptak@gmail.com>
2012-09-24 01:38:34 +0400

    wooopsss

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 if [ ! `which vim` ]; then 
   echo "Vim not installed, let's install it!"
   if [ `which yum` ]; then
-    sudo yum install -y vim
+    sudo yum install vim
   elif [ `which apt` ]; then
     sudo apt-get install vim
   fi
 fi
 
-if [ ! -d "~/.vim" ]; then
-  echo "No vim directory, figure out why! Peace!"
-  exit 1
-fi
+#if [ -d "~/.vim" ]; then
+#	echo "No vim directory, figure out why! Peace!"
+#	exit 1
+#fi
 
 ### BIN ###
-ln -s `readlink -f bin` ~/
+ln -sf `readlink -f bin` ~/
 
 ### VIM ###
-ln -s `readlink -f vimrc` ~/
+ln -sf `readlink -f vimrc` ~/.vimrc
 
 ### BASH ###
-ln -s `readlink -f bashrc` ~/
-ln -s `readlink -f bash_aliases` ~/
+ln -sf `readlink -f bashrc` ~/.bashrc
+ln -sf `readlink -f bash_aliases` ~/.bash_aliases
 
 ### TMUX ###
-ln -s `readlink -f tmux.cconf` ~/
+ln -sf `readlink -f tmux.conf` ~/.tmux.conf
 
 
 #Install Vundle cause VIM should manage VIMScripts, not git!
